e48bf320618fc3ffbb049423cbde8d6c8cbcd465
JavierBoscan <javierboscan16@gmail.com>
2020-08-20 22:25:41 -0400

    se agrego la libreria scrollreveal para efectos visuales junto con el archivo main.js para aplicar dicha libreria

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+ScrollReveal().reveal('.cta', { delay: 500 });
+ScrollReveal().reveal('.cards', { delay: 500 });
+ScrollReveal().reveal('#products', { delay: 500 });
+ScrollReveal().reveal('.video', { delay: 500 });
+ScrollReveal().reveal('.information', { delay: 500 });
+ScrollReveal().reveal('.questions', { delay: 500 });
+ScrollReveal().reveal('.map', { delay: 500 });
+ScrollReveal().reveal('.contact-data', { delay: 500 });
+ScrollReveal().reveal('.footer', { delay: 500 });
